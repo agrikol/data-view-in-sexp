aad639a09c482f59757e6f07f31bb53e1880a04f
agrikol <agrikol1995@yandex.ru>
2026-01-03 11:53:35 +0700

    Updated format.md

--- src/shared/model.py
+++ src/shared/model.py
@@ -120,29 +120,3 @@ class Node:
         else:
             children: str = " ".join(child.to_sexp() for child in self.children)
             return f"({" ".join(filter(None, [self.name, attrs, children]))})"
-
-
-# class Element(Node):
-#     def __init__(
-#         self,
-#         name: str,
-#         attrs: Dict[str, Any] | None = None,
-#         children: List[Union["Element", "Atom"]] | None = None,
-#     ):
-#         self.name = name
-#         self.attrs = attrs or {}
-#         self.children = children or []
-
-#     def __repr__(self):
-#         return f"Element({self.name}, attrs={self.attrs}, children={self.children})"
-
-
-# class Document:
-#     def __init__(self, root: Element):
-#         self.root = root
-
-#     def find(self, path: str) -> List[Node]:
-#         raise NotImplementedError("К реализации. @Игорь")
-
-#     def modify(self, path: str, new_value: Any):
-#         raise NotImplementedError("К реализации. @Игорь")
